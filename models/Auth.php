5aa308c49f40fa22e69dba9a1d92e682afb4a00d
Pedro Miguel Sousa <sousacmpedro@gmail.com>
2020-08-29 11:16:00 -0300

    Refaturando o Auth

--- models/Auth.php
+++ models/Auth.php
@@ -4,18 +4,19 @@ require_once 'dao/UserDaoMysql.php';
 class Auth {
     private $pdo;
     private $base;
+    private $dao;
 
     public function __construct(PDO $pdo, $base) {
         $this->pdo = $pdo;
-        $this->base = $base;        
+        $this->base = $base;
+        $this->dao = new UserDaoMysql($this->pdo);      
     }
 
     public function checkToken() {
         if(!empty($_SESSION['token'])) {
             $token = $_SESSION['token'];
-
-            $userDao = new UserDaoMysql($this->pdo);
-            $user = $userDao->findByToken($token);
+            
+            $user = $this->dao->findByToken($token);
 
             if($user) {
                 return $user;
@@ -26,10 +27,8 @@ class Auth {
         exit;
     }
 
-    public function validateLogin($email, $password) {
-        $userDao = new UserDaoMysql($this->pdo);
-
-        $user = $userDao->findByEmail($email);
+    public function validateLogin($email, $password) {  
+        $user = $this->dao->findByEmail($email);
         if($user) {
 
             if(password_verify($password, $user->password)) {
@@ -37,7 +36,7 @@ class Auth {
 
                 $_SESSION['token'] = $token;
                 $user->token = $token;
-                $userDao->update($user);
+                $this->dao->update($user);
 
                 return true;
             }
@@ -48,13 +47,10 @@ class Auth {
     }
 
     public function emailExists($email) {
-        $userDao = new UserDaoMysql($this->pdo);
-        return $userDao->findByEmail($email) ? true : false;
+        return $this->dao->findByEmail($email) ? true : false;
     }
 
     public function registerUser($name, $email, $password, $birthdate) {
-        $userDao = new UserDaoMysql($this->pdo);
-
         $hash = password_hash($password, PASSWORD_DEFAULT);
         $token = md5(time().rand(0, 9999));
 
@@ -65,7 +61,7 @@ class Auth {
         $newUser->birthdate = $birthdate;
         $newUser->token = $token;
 
-        $userDao->insert($newUser);
+        $this->dao->insert($newUser);
 
         $_SESSION['token'] = $token;
     }
