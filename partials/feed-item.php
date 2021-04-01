18cbe93b5646c30bc2a7baef4236deb6f21550b2
Pedro Miguel Sousa <sousacmpedro@gmail.com>
2021-04-01 06:13:57 -0300

    Bug, like do perfil

--- partials/feed-item.php
+++ partials/feed-item.php
@@ -3,10 +3,10 @@ require_once 'feed-item-script.php';
 
 $actionPhrase = '';
 switch($item->type) {
-    case 'text' :
+    case 'text':
         $actionPhrase = 'fez um post';
     break;
-    case 'photo' : 
+    case 'photo':
         $actionPhrase = 'postou uma foto';
     break;
 }
@@ -15,59 +15,61 @@ switch($item->type) {
     <div class="box-body">
         <div class="feed-item-head row mt-20 m-width-20">
             <div class="feed-item-head-photo">
-                <a href="<?= $base; ?>/profile.php?id=<?= $item->user->id; ?>"><img src="<?= $base; ?>/media/avatars/<?= $item->user->avatar; ?>" /></a>
+                <a href="<?=$base;?>/perfil.php?id=<?=$item->user->id;?>"><img src="<?=$base;?>/media/avatars/<?=$item->user->avatar;?>" /></a>
             </div>
             <div class="feed-item-head-info">
-                <a href="<?= $base; ?>/profile.php?id=<?= $item->user->id; ?>"><span class="fidi-name"><?= $item->user->name; ?></span></a>
-                <span class="fidi-action"><?= $actionPhrase; ?></span>
-                <br />
-                <span class="fidi-date"><?= date('d/m/Y H:i', strtotime($item->created_at)); ?></span>
+                <a href="<?=$base;?>/perfil.php?id=<?=$item->user->id;?>"><span class="fidi-name"><?=$item->user->name;?></span></a>
+                <span class="fidi-action"><?=$actionPhrase;?></span>
+                <br/>
+                <span class="fidi-date"><?=date('d/m/Y', strtotime($item->created_at));?></span>
             </div>
             <?php if($item->mine): ?>
                 <div class="feed-item-head-btn">
-                    <img src="<?= $base; ?>/assets/images/more.png" />
+                    <img src="<?=$base;?>/assets/images/more.png" />
                     <div class="feed-item-more-window">
                         <a href="<?=$base;?>/excluir_post_action.php?id=<?=$item->id;?>">Excluir Post</a>
                     </div>
-                </div>                
+                </div>
             <?php endif; ?>
         </div>
         <div class="feed-item-body mt-10 m-width-20">
-            <?php 
+            <?php
             switch($item->type) {
-                case 'text' :
+                case 'text':
                     echo nl2br($item->body);
                 break;
-                case 'photo' : 
+                case 'photo':
                     echo '<img src="'.$base.'/media/uploads/'.$item->body.'" />';
                 break;
             }
             ?>
         </div>
         <div class="feed-item-buttons row mt-20 m-width-20">
-            <div class="like-btn <?=$item->liked?'on':'';?>"><?=$item->likeCount; ?></div>
-            <div class="msg-btn"><?= count($item->comments)?></div>
+            <div class="like-btn <?=$item->liked?'on':'';?>"><?=$item->likeCount;?></div>
+            <div class="msg-btn"><?=count($item->comments);?></div>
         </div>
         <div class="feed-item-comments">
             <div class="feed-item-comments-area">
                 <?php foreach($item->comments as $comment): ?>
                     <div class="fic-item row m-height-10 m-width-20">
                         <div class="fic-item-photo">
-                            <a href="<?=$base;?>/profile.php?id=<?=$comment->id_user;?>"><img src="<?=$base;?>/media/avatars/<?=$comment->user->avatar; ?>" /></a>
+                            <a href="<?=$base;?>/perfil.php?id=<?=$comment->id_user;?>"><img src="<?=$base;?>/media/avatars/<?=$comment->user->avatar;?>" /></a>
                         </div>
                         <div class="fic-item-info">
-                            <a href="<?=$base;?>/profile.php?id=<?=$comment->id_user;?>"><?=$comment->user->name; ?></a>
-                            <?=$comment->body; ?>
+                            <a href="<?=$base;?>/perfil.php?id=<?=$comment->id_user;?>"><?=$comment->user->name;?></a>
+                            <?=$comment->body;?>
                         </div>
-                    </div>  
+                    </div>
                 <?php endforeach; ?>
             </div>
+
             <div class="fic-answer row m-height-10 m-width-20">
                 <div class="fic-item-photo">
-                    <a href="<?=$base;?>/profile.php?id=<?=$userInfo->id;?>"><img src="<?= $base; ?>/media/avatars/<?=$userInfo->avatar;?>" /></a>
+                    <a href="<?=$base;?>/perfil.php"><img src="<?=$base;?>/media/avatars/<?=$userInfo->avatar;?>" /></a>
                 </div>
                 <input type="text" class="fic-item-field" placeholder="Escreva um comentário" />
             </div>
+
         </div>
     </div>
 </div>
